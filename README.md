56b98d4d3991590d4bc0b085d190b5a06c13bf8e
Andrew Dai <theman@andrewdai.co>
2016-11-20 14:48:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CS4400
+## Installation
+
+```
+pip install -r requirements.txt
+```
+
+## Running the app
+
+```
+python manage.py runserver
+```
---
+++ app/controllers/api.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request, url_for, jsonify
+
+api = Blueprint('api', __name__)
+
+@api.route('/')
+def home():
+    return jsonify(hello='world')
