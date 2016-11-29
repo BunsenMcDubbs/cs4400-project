9fcfbc37c17a381de5229c7b48f1265943e08119
Andrew Dai <theman@andrewdai.co>
2016-11-29 15:27:42 -0500

    remove year population (should be in next migration script)

--- migrations/001-init.sql
+++ migrations/001-init.sql
@@ -14,13 +14,6 @@ create table year_name (
   primary key (year, name)
 );
 
-insert into year_name (year, name)
-values
-  (2017, 'Senior'),
-  (2018, 'Junior'),
-  (2019, 'Sophomore'),
-  (2020, 'Freshman');
-
 create table project (
   name varchar(50) primary key,
   description varchar(200),
