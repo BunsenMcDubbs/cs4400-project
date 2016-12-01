f496e89bd4bfeba1694d50caa0dc95a00a7d26f9
Andrew Dai <theman@andrewdai.co>
2016-12-01 01:26:45 -0500

    start/add `/admin/add_project` route and page

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -2,9 +2,17 @@ from flask import Blueprint, render_template, flash, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 
 # from app.models import User
+from app.forms import (
+    AddProjectForm
+)
 
 admin = Blueprint('admin', __name__,)
 
 @admin.route('/')
 def home():
     return render_template('admin/index.html')
+
+@admin.route('/add_project', methods=['GET', 'POST'])
+def add_project():
+    form = AddProjectForm()
+    return render_template('admin/add_project.html', form=form)
