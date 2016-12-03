bc550baed9273a13859db8a039bd278b42cdc11f
Andrew Dai <theman@andrewdai.co>
2016-12-03 00:18:15 -0500

    implement project applications

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for, abort
 from flask_login import login_user, logout_user, login_required, current_user
 
+from datetime import datetime
+
 from app.forms import LoginForm, RegisterUserForm, EditUserForm
-from app.models import User, Course, Project
+from app.models import User, Course, Project, Application
 from app.search import search
 
 main = Blueprint('main', __name__)
@@ -15,14 +17,34 @@ def home():
     results = search()
     return render_template('index.html', results=results)
 
-@main.route('/project/<project_name>')
+@main.route('/project/<project_name>', methods=['GET', 'POST'])
+@login_required
 def view_project(project_name):
     project = Project.find_by_name(project_name)
     if project is None:
         return abort(404)
-    return render_template('view_project.html', project=project)
+    has_prev_application = Application.find(
+        project_name=project_name,
+        student_name=current_user.username,) is not None
+    if request.method == 'POST':
+        if has_prev_application:
+            flash('You can only apply once to each project', 'danger')
+        else:
+            application = Application(
+                project_name=project_name,
+                student_name=current_user.username,
+                application_date=datetime.now(),
+                status='pending',
+                is_new_application=True)
+            application.save()
+            flash('Application successfully submitted!', 'success')
+    return render_template(
+        'view_project.html',
+        project=project,
+        has_prev_application=has_prev_application)
 
 @main.route('/course/<course_name>')
+@login_required
 def view_course(course_name):
     course = Course.find_by_name(course_name)
     if course is None:
